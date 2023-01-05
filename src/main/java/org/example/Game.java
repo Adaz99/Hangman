c3c271d9f8e6b6449900137fd7ad6c6585298a22
Airidas <airidassien64@gmail.com>
2023-01-05 15:39:45 +0000

    Created the classes needed

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Game {
+    private static ArrayList<String> wordList = new ArrayList<>();
+
+    static {
+        "wolf",
+        "destruction",
+        "lineage",
+        "confusion",
+        "staff",
+    };
+}
