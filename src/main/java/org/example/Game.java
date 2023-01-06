8dd1f09146224d45aed010f03722740fb4a82757
Airidas <airidassien64@gmail.com>
2023-01-06 16:15:02 +0000

    created a endgame method so when you have 0 lives left it ends

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -7,26 +7,58 @@ import java.util.Scanner;
 public class Game {
     String a;
     String changeWord;
-    String answer;
+    char answer[];
+    int lives = 8;
+
+    Scanner userInput = new Scanner(System.in);
+
     private void turn() {
-        Scanner userInput = new Scanner(System.in);
+        int count = 0;
 
         System.out.print("Enter a character: ");
 
         char c = userInput.next().charAt(0);
-
+        for (int i = 0; i < a.length(); i++) {
+            if (c == a.charAt(i)) {
+                answer[i] = c;
+                count++;
+            }
+        }
         System.out.println("You entered: " + c);
-
-
-        answer = a.replaceAll("[^ " + c + "]", " _ ");
         System.out.println(answer);
-        System.out.println(a);
-        turn();
+
+
+        if (count == 0) {
+            lives--;
+            System.out.println("Wrong guess!" + " You have " + lives + " lives left.");
+            if (lives == 0) {
+                endGame();
+            } else {
+                turn();
+            }
+            } else {
+            turn();
+            }
+
     }
-    public void start(){
+
+    public void start() {
         a = WordList.generateRandomWord();
+        String temp = "_".repeat(a.length());
+        answer = temp.toCharArray();
+        System.out.println("Welcome to the Hangman game! Enter a character to start");
+        System.out.println("You have " + lives + " lives left.");
         turn();
     }
 
+    public void endGame() {
+        System.out.println("Game over no lifes left!");
+        System.exit(0);
+
     }
+
+    public void winner() {
+        System.out.println("Game complete, WINNER!!!");
+    }
+}
 
